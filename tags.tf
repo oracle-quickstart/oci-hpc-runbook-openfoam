0d9d37d0ce584d7a497a2aed9a44bbbf2a17ddc4
Lukasz Feldman <lukasz.feldman@oracle.com>
2021-06-14 16:48:36 +0200

    Updated tags.tf

--- tags.tf
+++ tags.tf
@@ -9,7 +9,7 @@ resource "oci_identity_tag_namespace" "ArchitectureCenterTagNamespace" {
   provider       = oci.homeregion
   compartment_id = var.compartment_ocid
   description    = "ArchitectureCenterTagNamespace"
-  name           = "ArchitectureCenter\\oci-hp-notebook-openfoam-${random_id.tag.hex}"
+  name           = "ArchitectureCenter\\hpc-openfoam-${random_id.tag.hex}"
 
   provisioner "local-exec" {
     command = "sleep 10"
